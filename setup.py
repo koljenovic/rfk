1213e09a378fdca418dccb556ae9872a7bdcc4c6
koljenovic <koljenovic@gmail.com>
2021-07-22 11:26:45 +0200

    setup.py added for -e install

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+	name = "rfkadapter",
+	version = "0.1.0",
+	author = "Malik Koljenović",
+	author_email = "malik@mekom.ba",
+	description = "RFKAdapter emulira API nalik SQL jeziku (SELECT, UPDATE, CREATE) za DBF \"baze podataka\"",
+	long_description = long_description,
+	long_description_content_type = "text/markdown",
+	url = "https://gitea.lab.ba/mekom/rfk",
+	project_urls = {
+		"Bug Tracker": "https://gitea.lab.ba/mekom/rfk/issues"
+	},
+	classifiers =[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+
+	package_dir = { "": "src" },
+	packages = setuptools.find_packages(where="src"),
+	python_requires = ">=3.7",
+)
