c6346b1543b9b81f5491b83645c5b2e81c1af500
koljenovic <koljenovic@gmail.com>
2021-08-24 15:14:22 +0200

    version bump v0.3.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
 	name = "rfkadapter",
-	version = "0.3.1",
+	version = "0.3.2",
 	author = "Malik Koljenović",
 	author_email = "malik@mekom.ba",
 	description = "RFKAdapter emulates a SQL-like CRUD API for DBF",
